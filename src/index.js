7a29a6e7cd6addeaed4ba39062dc365e73616a20
Francisco-BT <pancho10810@gmail.com>
2021-12-13 17:56:35 -0600

    feat: change app to no use socket mode
    Add listener for actions and im messages

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
 const {
   SLACK_BOT_TOKEN,
   SLACK_SIGNING_SECRET,
   SLACK_APP_TOKEN,
-} = require('./config');
+} = require("./config");
 
 const app = new App({
   token: SLACK_BOT_TOKEN,
   signingSecret: SLACK_SIGNING_SECRET,
-  socketMode: true,
   appToken: SLACK_APP_TOKEN,
 });
 
 // Listens to incoming messages that contain "hello"
-app.message('hello', async (event) => {
-  console.log('EVENT: ', JSON.stringify(event.message, null, 2));
+app.message("hello", async (event) => {
+  console.log("EVENT: ", JSON.stringify(event.message, null, 2));
   const { message, say } = event;
   // say() sends a message to the channel where the event was triggered
   await say({
     blocks: [
       {
-        type: 'section',
+        type: "section",
         text: {
-          type: 'mrkdwn',
+          type: "mrkdwn",
           text: `Hey there <@${message.user}>!`,
         },
         accessory: {
-          type: 'button',
+          type: "button",
           text: {
-            type: 'plain_text',
-            text: 'Click Me',
+            type: "plain_text",
+            text: "Click Me",
           },
-          action_id: 'button_click',
+          action_id: "button_click",
         },
       },
     ],
@@ -39,15 +38,52 @@ app.message('hello', async (event) => {
   });
 });
 
-app.event('team_join', async (event) => {
-  console.log('Event: ', event.event);
-  console.log('Body: ', event.body);
-  console.log('Event say: ', event.say);
+app.event("team_join", async (event) => {
+  console.log("Event: ", event.event);
+  console.log("Body: ", event.body);
+  console.log("Event say: ", event.say);
+});
+
+app.event("member_joined_channel", async (event) => {
+  //console.log("Event data", JSON.stringify(event, null, 2));
+  //await event.say(
+  //  `Welcome <@${event.payload.user}> to this channel <@${event.payload.channel}>!`
+  //);
+
+  await app.client.chat.postMessage({
+    channel: event.payload.user,
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Welcome <@${event.payload.user}> to Arkus!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Go to Cultura Remota",
+          },
+          style: "primary",
+          url: "http://187.191.25.39:1045/",
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Welcome <@${event.payload.user}> to Arkus!`,
+  });
+});
+
+app.action("button_click", async (event) => {
+  const { body, ack, say } = event;
+  await ack();
+  //await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
   // Start your app
   await app.start(3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
